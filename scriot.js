780972550cf9be7d54c66051fa01812efad55b22
Medo <144485749+suiiiiiiiiiiii7@users.noreply.github.com>
2023-11-10 20:23:27 +0300

    Add files via upload

---
+++ scriot.js
@@ -0,0 +1,32 @@
+const api = "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";
+const inp = document.getElementById('inp');
+const images = document.querySelector('.images');
+
+const getImage = async () => {
+  const methods = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${api}`
+    },
+    body:JSON.stringify({
+      "prompt": inp.value,
+      "n": 3,
+      "size": "512x512"
+    }
+    )
+  }
+
+  const res = await fetch("https://api.openai.com/v1/images/generations", methods);
+  const data = await res.json();
+const listImages = data.data;
+images.innerHTML = ''
+listImages.map(photo => {
+const container = document.createElement("div")
+images.append(container)
+const img = document.createElement("img")
+container.append(img)
+img.src = photo.url
+
+})  
+}
